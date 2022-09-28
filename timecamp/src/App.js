69bc53057ad52c01e85d6aa63829f60b8151ecb9
Divyanka Rai <raidivyanka40@gmail.com>
2022-09-28 21:59:12 +0530

    day-2

--- timecamp/src/App.js
+++ timecamp/src/App.js
@@ -1,9 +1,14 @@
+import React from 'react';
 import './App.css';
+import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
+
 
 function App() {
   return (
-    <div className="App">
-    
+    <div className='App'>
+      <Navbar/>
+      <Footer/>
     </div>
   );
 }
