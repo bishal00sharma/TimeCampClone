3605e2eb8e04db1ae22b4333f8e6bf0a1f66b1ca
AbhayAg10 <abhaytalkin@gmail.com>
2022-09-28 20:21:23 +0530

    changes made

--- timecamp/src/App.js
+++ timecamp/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import TrelloTimeTracking from './Integrations/trelloTimeTracking/trelloTimeTracking';
 
 function App() {
   return (
     <div className="App">
-    
+      <TrelloTimeTracking />
     </div>
   );
 }
---
+++ timecamp/src/Integrations/TrelloTimeTracking/TrelloTimeTracking.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from "./TrelloTimeTracking.module.css";
+
+const TrelloTimeTracking = () => {
+  return (
+    <div className={styles.top} >TrelloTimeTracking</div>
+  )
+}
+
+export default TrelloTimeTracking
