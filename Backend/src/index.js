aec8c6a1ce9413adf183adf3ec2db1acf58a8333
lokesh-dc <lokesh.cdewanand@gmail.com>
2022-09-29 19:06:54 +0530

    environment setup

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,7 +1,9 @@
+require("dotenv").config({path: __dirname+"/config.env"})
 const express = require('express');
 const dbconnect = require("./config/dbConnect");
-const tasksRouter=require("./features/Tasks/tasks.router");
 
+
+const tasksRouter=require("./features/Tasks/tasks.router");
 const userRouter = require("./features/Users/users.router");
 const projectRouter = require("./features/Projects/projects.router");
 
@@ -12,12 +14,11 @@ app.use("/users", userRouter);
 app.use("/tasks",tasksRouter);
 app.use("/projects",projectRouter);
 
-
 app.get("/", (req, res)=>{
     res.send("working fine");
 })
 
 app.listen(8080, async ()=>{
     await dbconnect();
-    console.log("Listening on http://localhost:8080");
+    console.log(`Listening on http://localhost:${process.env.PORT}`);
 })
