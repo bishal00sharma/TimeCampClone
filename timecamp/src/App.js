dfc181922f514daf84826294a8a5d53bd6c100b0
AbhayAg10 <abhaytalkin@gmail.com>
2022-10-01 14:29:35 +0530

    module.css file added

--- timecamp/src/App.js
+++ timecamp/src/App.js
@@ -1,11 +1,12 @@
 import { Box } from '@chakra-ui/react';
 import React from 'react';
 import './App.css';
-import HomePage from './Routes/HomePage';
-// import TrelloTimeTracking from './Integrations/TrelloTimeTracking/TrelloTimeTracking';
+import GoogleCalender from './Pages/Integrations/GoogleCalender/GoogleCalender';
+import TrelloTimeTracking from './Pages/Integrations/TrelloTimeTracking/TrelloTimeTracking';
+// import HomePage from './Routes/HomePage';
 
 // import Features from './Pages/Features/Features';
-import Navbar from './Components/Navbar';
+// import Navbar from './Components/Navbar';
 // import Footer from './Components/Footer';
 // import Pricing from './Routes/Pricing';
 // import Automatic from './Pages/Features/Automatic';
@@ -13,11 +14,12 @@ function App() {
   return (
     <div className="App">
     {/* <Features/> */}
-    <Navbar/>
+    {/* <Navbar/> */}
     {/* <HomePage/> */}
       {/* <Footer/> */}
-      <Pricing/>
+      {/* <Pricing/> */}
       {/* <TrelloTimeTracking /> */}
+      <GoogleCalender />
       {/* <Automatic/> */}
    </div>
   );
